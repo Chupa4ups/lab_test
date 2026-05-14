4dffa6a020eb5bc71805890fcae4e18fbd9d7d53
Chupa4ups <SOLO144444@yandex.ru>
2026-05-14 18:06:39 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 1200,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 1000,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 500,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Михаил Лермонтов',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Мёртвые души',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Иван Тургенев',
+        name: 'Отцы и дети',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Борис Пастернак',
+        name: 'Доктор Живаго',
+        publicationYear: 2026,
+        price: 700,
+        bestseller: false,
+        available: false
+    },
+];
