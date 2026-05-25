dd8599cd4dd597b6ce1418f9d77d7ecf16580277
Chupa4ups <SOLO144444@yandex.ru>
2026-05-25 23:47:48 +0700

    Дополнительное задание

---
+++ lab6/planets.js
@@ -0,0 +1,73 @@
+let planets = [
+
+    {
+        name: 'Меркурий',
+        radius: '2440 км',
+        temperature: '167°C',
+        image: 'Mercury.jpg'
+    },
+
+    {
+        name: 'Венера',
+        radius: '6052 км',
+        temperature: '464°C',
+        image: 'Venera.jpg'
+    },
+
+    {
+        name: 'Земля',
+        radius: '6371 км',
+        temperature: '15°C',
+        image: 'Earth.jpg'
+    },
+
+    {
+        name: 'Марс',
+        radius: '3390 км',
+        temperature: '-65°C',
+        image: 'Mars.jpg'
+    },
+
+    {
+        name: 'Юпитер',
+        radius: '69911 км',
+        temperature: '-110°C',
+        image: 'Jupiter.jpg'
+    },
+
+    {
+        name: 'Сатурн',
+        radius: '58232 км',
+        temperature: '-140°C',
+        image: 'Saturn.jpg'
+    },
+
+    {
+        name: 'Уран',
+        radius: '25362 км',
+        temperature: '-195°C',
+        image: 'Uranus.jpg'
+    },
+
+    {
+        name: 'Нептун',
+        radius: '24622 км',
+        temperature: '-200°C',
+        image: 'Neptune.jpg'
+    },
+
+    {
+        name: 'Плутон',
+        radius: '1188 км',
+        temperature: '-225°C',
+        image: 'Pluton.jpg'
+    },
+
+    {
+        name: 'Эрида',
+        radius: '1163 км',
+        temperature: '-243°C',
+        image: 'Erida.jpg'
+    }
+
+];
